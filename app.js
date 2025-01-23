25e52015c689c09a2b5e7bcede3ae4076dee29ff
Victor LEFÈVRE <victor.lefevre@etu.unicaen.fr>
2025-01-23 20:52:42 +0100

    Update Vercel configuration and package.json scripts for improved deployment

--- app.js
+++ app.js
@@ -54,12 +54,12 @@ app.use('/users', user);
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -70,3 +70,7 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+if (process.env.VERCEL) {
+  app.listen(process.env.PORT || 3000);
+}
