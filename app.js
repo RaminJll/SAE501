8b2aa72a8a33162e7cadea492a5e42daa53e4354
Victor LEFÈVRE <victor.lefevre@etu.unicaen.fr>
2025-01-24 10:09:20 +0100

    Revert to working version

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -54,12 +54,12 @@ app.use('/users', user);
 
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -68,9 +68,5 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
-if (process.env.VERCEL) {
-  app.listen(process.env.PORT || 3000);
-}
 
 module.exports = app;
