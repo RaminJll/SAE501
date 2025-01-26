75ab9376db5926b29069a60ddb50c9142be1c123
Victor LEFÈVRE <victor.lefevre@etu.unicaen.fr>
2025-01-26 17:21:46 +0100

    Middleware

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
 var express = require('express');
-const middleware = require('./middleWare');
+const middleware = require('../middleware/middleware');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', middleware, function(req, res, next) {
+router.get('/', middleware, function (req, res, next) {
   res.json({
     message: 'Vous êtes connecté',
     user: req.user
